0f821d12dbf4bc7346cd94e3d430780c186d7e35
fer77vaca <ferchi77vaca@gmail.com>
2023-10-28 13:51:06 -0400

    Se corrigio el tipo de dato de {} a string en dto de catalogos

--- libreria/src/categorias/dto/create-categoria.dto.ts
+++ libreria/src/categorias/dto/create-categoria.dto.ts
@@ -6,17 +6,17 @@ export class CreateCategoriaDto {
     @IsNotEmpty({ message: 'El campo nombre no debe ser vacio' })
     @IsString({ message: 'El campo nombre debe ser de tipo cadena' })
     @MaxLength(50, { message: 'El campo nombre no debe ser mayor a 50 caracteres' })
-    readonly nombre;
+    readonly nombre : string;
 
     @ApiProperty()
     @IsNotEmpty({ message: 'El campo marca no debe ser vacio' })
     @IsString({ message: 'El campo marca debe ser de tipo cadena' })
     @MaxLength(50, { message: 'El campo marca no debe ser mayor a 50 caracteres' })
-    readonly marca;
+    readonly marca : string;
 
     @ApiProperty()
     @IsNotEmpty({ message: 'El campo descripcion no debe ser vacio' })
     @IsString({ message: 'El campo descripcion debe ser de tipo cadena' })
     @MaxLength(200, { message: 'El campo descripcion no debe ser mayor a 50 caracteres' })
-    readonly descripcion;
+    readonly descripcion : string;
 }
