2d51c478ccfbf769cc2ace80ca772f26693f4263
QuispeJoseDavid <130864498+JoseDavidFull@users.noreply.github.com>
2023-11-02 18:08:17 -0600

    Agrega definición de interfaz Cliente y de Categoria

---
+++ libreriavue/src/models/categoria.ts
@@ -0,0 +1,6 @@
+export interface Categoria{
+    nombre: string
+    marca : string
+    descripcion : string
+}
+  
---
+++ libreriavue/src/models/cliente.ts
@@ -0,0 +1,10 @@
+export interface Cliente{
+    id: number
+    nombre : string
+    telefono : number
+    nit : number
+    razonSocial : string
+    direccion : string
+    email : string
+ }
+  
