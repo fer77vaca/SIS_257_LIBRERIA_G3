4eae11bf7a8fd3074e45c3d1d027e37c900a9bba
fer77vaca <ferchi77vaca@gmail.com>
2023-11-14 20:05:51 -0400

    entity

--- libreria/src/clientes/entities/cliente.entity.ts
+++ libreria/src/clientes/entities/cliente.entity.ts
@@ -1,31 +1,37 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('clientes')
 export class Cliente {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ type: 'varchar', length: 50, nullable: false })
-    nombre: string;
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  nombre: string;
 
-    @Column({ name: 'telefono', type: 'int' })
-    telefono: number;
+  @Column({ name: 'telefono', type: 'int' })
+  telefono: number;
 
-    @Column({ name: 'NIT', type: 'int' })
-    nit: number;
+  @Column({ name: 'NIT', type: 'int' })
+  nit: number;
 
-    @Column({ type: 'varchar', length: 100, nullable: false })
-    razonSocial: string;
+  @Column({ type: 'varchar', length: 100, nullable: false })
+  razonSocial: string;
 
-    @Column({ type: 'varchar', length: 200, nullable: false })
-    direccion: string;
+  @Column({ type: 'varchar', length: 200, nullable: false })
+  direccion: string;
 
-    @Column({ type: 'varchar', length: 100, nullable: true })
-    email: string;
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  email: string;
+  // esto esGIGRIGJIGJRIGJRIGJ
+  @CreateDateColumn({ name: 'fecha_creacion' })
+  fechaCreacion: Date;
 
-    @CreateDateColumn({ name: 'fecha_creacion' })
-    fechaCreacion: Date;
-
-    @UpdateDateColumn({ name: 'fecha_modificacion' })
-    fechaModificacion: Date;
+  @UpdateDateColumn({ name: 'fecha_modificacion' })
+  fechaModificacion: Date;
 }
